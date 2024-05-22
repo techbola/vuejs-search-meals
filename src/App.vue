96c02dcc2e1d67b9451c307638c9af3f1b3efc88
Adebola <adebola@poptin.com>
2024-05-22 12:08:11 +0100

    setup vuex - state management

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
+<template>
+  <router-view />
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</template>
 
 <style scoped>
 
