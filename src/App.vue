f903fafe542dbb2a7144511544c2e13a25dc07e4
Adebola <adebola@poptin.com>
2024-05-22 13:20:20 +0100

    app layout design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <router-view />
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link :to="{name: home}" class="inline-flex items-center px-2 h-full">
+        Home
+      </router-link>
+
+      <div class="flex items-center gap-1">
+        <router-link :to="{name: home}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+          Search Meals
+        </router-link>
+        <router-link :to="{name: home}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+          Meals By Letter
+        </router-link>
+        <router-link :to="{name: home}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+          Meals By Ingredients
+        </router-link>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script setup>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
-    <pre>{{ meals }}</pre>
+    <div class="flex flex-col p-8">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals" />
+        <div class="flex justify-center gap-2 mt-2">
+            <router-link v-for="letter in letters" :key="letter" :to="{ name: 'byLetter', params: { letter } }">
+                {{  letter }}
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient";
 
-const meals = computed(() => {
-    return store.state.meals
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+    const response = await axiosClient.get("list.php?i=list");
+    console.log('data', response.data);
+    ingredients.value = response.data;
 })
 </script>
